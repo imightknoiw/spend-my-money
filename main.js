8d40722bd9502a2449d2a63adf30c272349a063a
alt <83017202+imightknoiw@users.noreply.github.com>
2022-11-02 08:05:45 -0400

    Update main.js

--- main.js
+++ main.js
@@ -49,11 +49,11 @@ function formatDate(dateString) {
 	var items = document.getElementsByClassName('item');
 	for (var i = 0; i < items.length; i++) {
 		var label = items[i].children[0];
-		alert(label.tagName);
+		alert(label.tagName + i.toString());
 		var button = items[i].children[2];
-		alert(button.tagName);
+		alert(button.tagName + i.toString());
 		var input = items[i].children[3];
-		alert(input.tagName);
+		alert(input.tagName + i.toString());
 		
 		label.innerText = 'Item ' + (i + 1).toString();
 		label.addAttribute('for', itemName);
